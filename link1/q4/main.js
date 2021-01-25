6ed343b6d97cc11655999c76431aa74b8abff3fd
guy586 <guy586@gmail.com>
2021-01-25 19:36:04 +0200

    solution to q4
    with help.

---
+++ link1/q4/main.js
@@ -0,0 +1,19 @@
+const field = document.getElementById("field");
+const ball = document.getElementById("ball");
+field.onclick = function(event) {
+    let fieldCoords = this.getBoundingClientRect();
+    let topc = event.clientY - fieldCoords.top - field.clientTop - ball.clientHeight / 2;
+    let leftc = event.clientX - fieldCoords.left - field.clientLeft - ball.clientWidth / 2;
+    if (topc < 0) {
+        topc = 0;
+    } if (leftc < 0) {
+        leftc = 0;
+    } if (leftc + ball.clientWidth > field.clientWidth) {
+        leftc = field.clientWidth - ball.clientWidth;
+    } if (topc + ball.clientHeight > field.clientHeight) {
+        topc = field.clientHeight - ball.clientHeight;
+    }
+
+    ball.style.top = `${topc}px`;
+    ball.style.left = `${leftc}px`;
+}
